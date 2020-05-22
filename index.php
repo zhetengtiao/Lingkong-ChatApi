68d30d78ac135ba6f2d02dc0b068d751ff5be8c4
zhetengtiao <51787359+zhetengtiao@users.noreply.github.com>
2020-05-22 10:41:43 +0800

    更新了新版本：1.1.3
    优化了cpu与内存，更加轻量
    测试cpu：英特尔酷睿i5-7200u，2.50GHz，2.70GHz
    内存ram：7.81GB
    操作系统：Windows10专业版，64位
    测试环境：PHPStudyPro
    （戴尔灵越7560）
    测试前cpu：6%
    测试前内存：3.6GB
    测试后cpu：6%
    测试后内存：3.61GB
    没有太大的变化

--- index.php
+++ index.php
@@ -2,38 +2,28 @@
 /**
  * Lingkong-ChatApi
  * Powerd By Zhetengtiao-LKT
- * 2020.5.19
- *
- *  What you laughed at me yesterday, I turned it into motivation today.
- *  And I will never forget the "power" you gave me.
- *  Don't forget that there are more powerful people besides you, and don't be arrogant.
- *  Otherwise that's how you get in the way of yourself.
- *  I hope you can remember that,Don't be like them.
- *  :)
- *
+ * 2020.5.22
  */
 //--------------------------------------------------------配置
-$passwd = "Ab1234";//passwd，管理员密码，用于创建apikey，使用前最好改一下
-$servername = "localhost";//servername，数据库连接地址
-$username = "root";//username，用户名
-$password = "123456";//password，密码
-$dbname = "ChatApi";//dbname,如果是系统自动创建那么就不用改
+$passwd = "Ab1234"; //passwd，管理员密码，用于创建apikey，使用前最好改一下
+$servername = "localhost"; //servername，数据库连接地址
+$username = "root"; //username，用户名
+$password = "123456"; //password，密码
+$dbname = "ChatApi"; //dbname,如果是系统自动创建那么就不用改
 //--------------------------------------------------------配置
-$version = "1.1.2";
+$version = "1.1.3";
 $o = $_GET["o"];
 $r = $_GET["r"];
 $apikey = $_GET["apikey"];
 $passwd1 = $_GET["passwd"];
 $text = $_GET["text"];
-function qrand($len)
-{
-    $chars='ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz';
-    $string=time();
-    for(;$len>=1;$len--)
-    {
-        $position=rand()%strlen($chars);
-        $position2=rand()%strlen($string);
-        $string=substr_replace($string,substr($chars,$position,1),$position2,0);
+function qrand($len) {
+    $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz';
+    $string = time();
+    for (; $len >= 1; $len--) {
+        $position = rand() % strlen($chars);
+        $position2 = rand() % strlen($string);
+        $string = substr_replace($string, substr($chars, $position, 1) , $position2, 0);
     }
     return $string;
 }
@@ -46,9 +36,7 @@ switch ($o) {
             $conn = new mysqli($servername, $username, $password);
             // 创建数据库
             $sql = "CREATE DATABASE ChatApi";
-            if ($conn->query($sql) === FALSE) {
-                echo "Error creating database: " . $conn->error;
-            }
+            $conn->query($sql);
             $conn->close();
             echo "Done!";
         } else {
@@ -57,47 +45,20 @@ switch ($o) {
         }
         return 0;
     case "n":
-        if (empty($apikey))
-        {
-            echo "No apikey!";
-            return 0;
-        }
-        if (empty($text))
-        {
-            echo "No text!";
-            return 0;
-        }
         $conn = new mysqli($servername, $username, $password, $dbname);
-        // 检测连接
-        if ($conn->connect_error) {
-            die("连接失败: " . $conn->connect_error);
-        }
         $time = date("Y-m-d");
-        $sql="insert into $apikey (text) values('$text')";
-        if ($conn->query($sql) === TRUE) {
-            echo "Done!";
-        } else {
-            echo "Error:" . $conn->error;
-        }
+        $sql = "insert into $apikey (text) values('$text')";
+        $conn->query($sql);
         $conn->close();
         return 0;
     case "l":
-        if (empty($apikey))
-        {
-            echo "No apikey!";
-            return 0;
-        }
         $conn = new mysqli($servername, $username, $password, $dbname);
-        // 检测连接
-        if ($conn->connect_error) {
-            die("连接失败: " . $conn->connect_error);
-        }
         $sql = "SELECT * FROM $apikey;";
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
-            $row1=array();
-            while($row = $result->fetch_assoc()) {
-                array_push($row1,$row);
+            $row1 = array();
+            while ($row = $result->fetch_assoc()) {
+                array_push($row1, $row);
             }
             echo json_encode($row1);
         } else {
@@ -109,18 +70,11 @@ switch ($o) {
         if ($passwd1 === $passwd) {
             $apkey = qrand(rand(1, 10));
             $conn = new mysqli($servername, $username, $password, $dbname);
-            // 检测连接
-            if ($conn->connect_error) {
-                die("连接失败: " . $conn->connect_error);
-            }
             $sql = "CREATE TABLE " . $apkey . " (
 TEXT VARCHAR(30) NOT NULL,
 reg_date TIMESTAMP
 )";
-            if ($conn->query($sql) === FALSE)
-            {
-                echo "Error creating database: " . $conn->error;
-            }
+            $conn->query($sql);
             $conn->close();
             echo $apkey;
             return 0;
@@ -128,8 +82,8 @@ reg_date TIMESTAMP
             echo "Admin Password Error!";
             return 0;
         }
-    default :
+    default:
         echo "Waring : No parameters passed in.";
-        echo "Lingkong_ChatApi-Powerd By Zhetengtiao. Version " . $version;
+        return 0;
 }
-?>
+?>       
